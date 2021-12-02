585d4b5ed4ab93daabe6703bca6c00207450f489
Grant Gordon <grant.gordon@hey.com>
2021-12-02 14:50:16 -0500

    define all variables; use let instead of set for inner loop stuff; better way of reading file

--- day2/puzzle2.lisp
+++ day2/puzzle2.lisp
@@ -3,58 +3,52 @@
 ; I arrived at this and `uiop:split-string` after some trial and error.
 (require "asdf")
 
+(defvar instructions)
 (set 'instructions
-  (prog1
-    (set 'dm (list()))
-      (set 'infile (open "puzzle2.input"))
-        (when infile
-	  (loop for line = (read-line infile nil)
-	    while line do (push line (cdr (last dm)))))))
+  (with-open-file (stream "puzzle2.input")
+    (loop for line = (read-line stream nil)
+      while line
+      collect line)))
 
-; because of the way I'm reading the file in, the first entry in the list is
-; an empty list. pop that off, so we only have instructions. 
-(set 'instructions (remove (nth 0 instructions) instructions))
-
+(defvar horizontal-position)
+(defvar depth)
 ; part 1
 (set 'horizontal-position 0)
 (set 'depth 0)
 (loop for instruction in instructions do
-      (set 'instruction-string 
-	   (nth 0 (uiop:split-string instruction :separator " ")))
-      (set 'instruction-count
-	   (parse-integer (nth 1 (uiop:split-string instruction :separator " "))))
+      (let ((instruction-string (nth 0 (uiop:split-string instruction :separator " ")))
+	    (instruction-count  (parse-integer (nth 1 (uiop:split-string instruction :separator " ")))))
 
-      ; forward adds x to horizontal-position
-      ; down adds x to depth
-      ; up subtracts x from depth
-      (if (search "forward" instruction-string)(set 'horizontal-position (+ horizontal-position instruction-count))
-	(if (search "down" instruction-string)(set 'depth (+ depth instruction-count))
-	  (if (search "up" instruction-string)(set 'depth (- depth instruction-count))))))
+        ; forward adds x to horizontal-position
+        ; down adds x to depth
+        ; up subtracts x from depth
+        ; `cond` might be a better fit here: http://clhs.lisp.se/Body/m_cond.htm
+        (if (search "forward" instruction-string)(set 'horizontal-position (+ horizontal-position instruction-count))
+	  (if (search "down" instruction-string)(set 'depth (+ depth instruction-count))
+	    (if (search "up" instruction-string)(set 'depth (- depth instruction-count)))))))
 
-; part 1 solution
-(write (* horizontal-position depth))(terpri)
+(format T "First calculation :: ~d" (* horizontal-position depth))(terpri)
 
 
 ; part 2
 (set 'horizontal-position 0)
 (set 'depth 0)
+
+(defvar aim)
 (set 'aim 0)
 (loop for instruction in instructions do
-      (set 'instruction-string 
-	   (nth 0 (uiop:split-string instruction :separator " ")))
-      (set 'instruction-count
-	   (parse-integer (nth 1 (uiop:split-string instruction :separator " "))))
+  (let ((instruction-string (nth 0 (uiop:split-string instruction :separator " ")))
+	(instruction-count  (parse-integer (nth 1 (uiop:split-string instruction :separator " ")))))
 
       ; down and up only adjust aim
-      (if (search "down" instruction-string)(set 'aim (+ aim instruction-count))
-	(if (search "up" instruction-string)(set 'aim (- aim instruction-count))))
+    (if (search "down" instruction-string)(set 'aim (+ aim instruction-count))
+      (if (search "up" instruction-string)(set 'aim (- aim instruction-count))))
 
-      ; forward adds count to horizontal-position
-      ; forward also adds (forward-count * aim) to depth
-      (if (search "forward" instruction-string)
-	(progn
-	  (set 'horizontal-position (+ horizontal-position instruction-count))
-	  (set 'depth (+ depth (* instruction-count aim))))))
+        ; forward adds count to horizontal-position
+        ; forward also adds (forward-count * aim) to depth
+        (if (search "forward" instruction-string)
+	  (progn
+	    (set 'horizontal-position (+ horizontal-position instruction-count))
+	    (set 'depth (+ depth (* instruction-count aim)))))))
 
-; part 2 solution
-(write (* horizontal-position depth))(terpri)
+(format T "Second calculation :: ~d" (* horizontal-position depth))(terpri)
