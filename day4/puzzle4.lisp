f3a9c587304ca831c23205696233a59f28543c5d
Grant Gordon <grant.gordon@hey.com>
2021-12-04 14:53:33 -0500

    answer to part 1

--- day4/puzzle4.lisp
+++ day4/puzzle4.lisp
@@ -7,11 +7,29 @@
 ; the bingo values are space-delimited
 (defvar bingo-tables)
 
+; TODO: This creates an array of 100 references to the same 5x5 array.
+; I think I can use :element-type to initialize it correctly. In the
+; meantime. 
 (set 'bingo-tables 
      (make-array 100
 		 :initial-element (make-array '(5 5))))
 
-; a bingo table is a 5x5 
+(defun read-bingo-line-into-bingo-table (bingo-table bingo-table-row-index line)
+  (let ((bingo-row-iterator 0))
+    (loop for bingo-number in (uiop:split-string line :separator " ")
+	  do
+
+		   ; TODO: in our input, single-digit numbers are preceded by 
+		   ; two spaces, rather than one. There's the delimiting space,
+		   ; and the space where a number in the tens column would be.
+		   ; I'm sure there's a better way to handle this but, for now,
+		   ; we will just skip when bingo-number is an empty string.
+	  (if (equal bingo-number "")() ;skip mis-parsed space
+	    (progn
+	      (setf (aref bingo-table bingo-table-row-index bingo-row-iterator) bingo-number)
+	      (incf bingo-row-iterator))))))
+	    
+
 
 (with-open-file (stream "puzzle4.input")
   (let ((bingo-table-index -1)
@@ -19,39 +37,99 @@
   (loop for line = (read-line stream nil)
 	while line
         do 
+
 	      ; if this is a newline:
 	(cond ((eql (length line) 0)
 	       ; start work on the next bingo table
 	       (incf bingo-table-index)
 	       ; start working on index 0 of the new bingo table
-	       (setf bingo-table-row-index 0))
+	       (setf bingo-table-row-index 0)
+	       ; create a *new* table (not a reference to the same one)
+	       (setf (aref bingo-tables bingo-table-index) (make-array '(5 5))))
 
 	      ; if this is the input line, store it in bingo-inputs
 	      ((eql (aref line 2) #\,)
-	       (set 'bingo-inputs line))
+	       (set 'bingo-inputs 
+		    (uiop:split-string line :separator ",")))
 
 	      ; if this line describes some bingo entries
 	      ((eql (aref line 2) #\ )
 	       (progn
-		 ; bingo-table is the table we're currently working on
-		 (let ((bingo-table (aref bingo-tables bingo-table-index))
-		       ; bingo-row-iterator is the table row (0 - 4) that we're working on
-		       (bingo-row-iterator 0))
-		   ; for each space-delimited number in the bingo entries
-		   ; TODO: in our input, single-digit numbers are preceded by 
-		   ; two spaces, rather than one. There's the delimiting space,
-		   ; and the space where a number in the tens column would be.
-		   ; I'm sure there's a better way to handle this but, for now,
-		   ; we will just skip when bingo-number is an empty string.
-		   (loop for bingo-number in (uiop:split-string line :separator " ")
-			 do
-			 (if (equal bingo-number "")() ; skip mis-parsed space
-			   (progn
-			     (setf (aref bingo-table bingo-table-row-index bingo-row-iterator) bingo-number)
-			     (incf bingo-row-iterator))))
-		 (incf bingo-table-row-index))))))))
-	      
+		 (read-bingo-line-into-bingo-table
+		   (aref bingo-tables bingo-table-index) 
+		   bingo-table-row-index
+		   line)
+		 (incf bingo-table-row-index)))))))
 
-(print bingo-inputs)(terpri)
-(print bingo-tables)(terpri)
+
+(defun bingo-table-row-is-solved (bingo-table)
+  ; if all 5 entries in a row have an "x", the bingo table is solved
+  ; don't need to know "in a row" just need to know that count for the row is 5
+  ; look at each row in the table
+  (let ((table-has-a-solved-row ()))
+    (loop for row from 0 upto 4
+	  do
+	  ; look at each column in the row
+	  (let ((count-of-x-in-row 0))
+	    (loop for i from 0 upto 4
+		  do (if (equal (aref bingo-table row i) "x")(incf count-of-x-in-row)()))
+	    (if (eql count-of-x-in-row 5)(setf table-has-a-solved-row t))))
+    (eval table-has-a-solved-row)))
+
+(defun bingo-table-column-is-solved (bingo-table)
+  (let ((table-has-a-solved-column ()))
+    ; look at each column in the table
+    (loop for column from 0 upto 4
+	  do
+	  (let ((count-of-x-in-column 0))
+	    ; look at each row
+	    (loop for row from 0 upto 4
+		  do (if (equal (aref bingo-table row column) "x")(incf count-of-x-in-column)()))
+	    (if (eql count-of-x-in-column 5)(setf table-has-a-solved-column t))))
+    (eval table-has-a-solved-column)))
+
+(defun bingo-table-is-solved (bingo-table)
+  (or 
+    (bingo-table-row-is-solved bingo-table)
+    (bingo-table-column-is-solved bingo-table)))
+
+(defun cross-out-bingo-table-input (bingo-table input)
+  (loop for row from 0 upto 4
+	do (loop for column from 0 upto 4
+		 do (if (equal (aref bingo-table row column) input)
+		      (setf (aref bingo-table row column) "x")
+		      ()))))
+
+(defun score-bingo-table (bingo-table)
+  (let ((sum 0))
+    (loop for row from 0 upto 4
+	  do (loop for column from 0 upto 4
+		do 
+		(if (equal (aref bingo-table row column) "x")
+		     () ; no points for "x"
+		     (incf sum (parse-integer (aref bingo-table row column))))))
+    (eval sum)))
+  
+		
+
+(defvar first-winner)
+(set 'first-winner ())
+(loop for bingo-input in bingo-inputs
+      do
+      ; cross out this input on each bingo table
+      (terpri)(terpri)
+      (loop for bingo-table-index from 0 upto (- (length bingo-tables) 1)
+	    do
+	    (let ((bingo-table (aref bingo-tables bingo-table-index)))
+	      (cross-out-bingo-table-input bingo-table bingo-input)))
+
+      ; check to see if there are any winners
+      (loop for bingo-table-index from 0 upto (- (length bingo-tables) 1)
+	    do
+	    (let ((bingo-table (aref bingo-tables bingo-table-index)))
+	      (if (bingo-table-is-solved bingo-table)
+		(print (* (score-bingo-table bingo-table) (parse-integer bingo-input)))
+		()))))
+
+
 
