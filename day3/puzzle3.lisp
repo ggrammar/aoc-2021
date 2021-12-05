00d255610634b703dd85820dd231410d019b8533
Grant Gordon <grant.gordon@hey.com>
2021-12-05 17:13:24 -0500

    add some work on part 2

--- day3/puzzle3.lisp
+++ day3/puzzle3.lisp
@@ -30,8 +30,14 @@
 	  (if (eq b 10)
 	    (progn
 	      (setf char-iterator 0)
-	      (set 'total-input-lines (+ total-input-lines 1)))())))))
+	      (set 'total-input-lines (+ total-input-lines 1)))
+	    ())))))
 
+(write result-array)(terpri)
+
+; TODO: Figure out how to just collect 1s and 0s from this loop, so
+; we can handle this without the calls to `write`. It will be useful
+; to have these values available for part 2. 
 (format T "gamma rate (binary)   :: ")
 (loop for i from 0 upto 11
       collect
@@ -43,5 +49,84 @@
       (if (> (aref result-array i) 500)(write 0)(write 1)))(terpri)
 
 ; I manually converted these to decimal to multiply them.
-(write (* 1836 2259))
+(format T "power consumption (decimal) :: ~d" (* 1836 2259))(terpri)
+
+
+; oxygen generator rating
+;   what is the most common value for this bit position? (found in gamma rate)
+;     keep only numbers with that bit in that position. 
+
+; co2 scrubber rating
+;   what is the least common value for this bit position? (found in epsilon rate)
+;     keep only numbers with that bit in that position
+
+(defvar filter-list)
+(set 'filter-list
+     (with-open-file (stream "puzzle3.input")
+       (loop for line = (read-line stream nil)
+	     while line
+	     collect line)))
+
+; this should be the only place a char actually appears
+(defun line-at-index-is-one (line index)
+  (declare (vector line))
+  (declare (number index))
+  (equal (aref line index) #\1))
+       
+(defun get-value-at-index (line index)
+  (declare (vector line))
+  (declare (number index))
+  (if (line-at-index-is-one line index)
+    (eval 1)
+    (eval 0)))
+
+(defun get-popular-at-index (some-list index)
+  (declare (list some-list))
+  (declare (number index))
+  ; sum the numbers at that index
+  (let (( some-sum (apply '+ (map 'list (lambda (x) (get-value-at-index x index)) some-list)))
+	( half-of-list (/ (length some-list) 2)))
+    (if (> some-sum half-of-list)
+      (eval 1)
+      (eval 0))))
+
+; TODO: Handle the situation where 0 and 1 are equally common. 
+(let ((tmp-filter-list filter-list))
+  (loop for i from 0 to 11 do
+	(print (length tmp-filter-list))
+	(setf tmp-filter-list 
+	      (remove-if 
+		(lambda (line) 
+		  (equal 
+		    (if (line-at-index-is-one line i)(eval 1)(eval 0))
+		    (get-popular-at-index tmp-filter-list i)))
+		tmp-filter-list))
+	(print (length tmp-filter-list))))
+
+
+; (print (apply '+ (map 'list (lambda (x) (get-value-at-index x 0)) filter-array)))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
