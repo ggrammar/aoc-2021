e4da1ca072271554f103ea5480c1ae92574fe921
Grant Gordon <grant.gordon@hey.com>
2021-12-06 10:17:40 -0500

    add new algo and solution for part 2

--- day6/puzzle6.lisp
+++ day6/puzzle6.lisp
@@ -1,33 +1,30 @@
-; needs to be run with `--control-stack-size 5` to increase the control stack size,
-; otherwise it bails around day 75. 
-
 (require "asdf")
 
+(defvar initial-fish)
+(set 'initial-fish
+     (make-array 300 :initial-contents
+		 (with-open-file (stream "puzzle6.input")
+		   (map 'list 'parse-integer (uiop:split-string (read-line stream nil) :separator ",")))))
+
 (defvar fish)
+(set 'fish (make-array 9))
 
-(set 'fish 
-     (with-open-file (stream "puzzle6.input")
-       (map 'list 'parse-integer (uiop:split-string (read-line stream nil) :separator ","))))
+(loop for i from 0 to 8 do
+      (setf (aref fish i) (length (remove-if-not (lambda (x) (equal x i)) initial-fish))))
 
-(defvar num-days)
-(set 'num-days 80)
+(defun generation (a)
+  (declare (array a))
+  (let ((spawning-fish (aref a 0)))
+    (loop for i from 0 upto 7 do
+	  (setf (aref a i)(aref a (+ 1 i))))
+    (setf (aref a 8) spawning-fish)
+    (incf (aref a 6) spawning-fish)))
 
-(defun iterate-fish (f) (- f 1))
-(defun reset-timer (f) (if (= f -1)(eval 6)(eval f)))
+(loop for day from 0 to 80 do (generation fish))
+(format T "After 80 days, there are ~d fish." (apply '+ (loop for i from 0 to 7 collect (aref fish i))))(terpri)
 
-(format T "Initial state, there are ~d fish." (length fish))(terpri)
-(let ((fish-to-populate 0))
-  (loop for day from 1 to num-days do
-	; decrement every fish
-	(set 'fish (map 'list (lambda (x) (iterate-fish x)) fish))
+(loop for day from 81 to 256 do (generation fish))
+(format T "After 256 days, there are ~d fish." (apply '+ (loop for i from 0 to 7 collect (aref fish i))))(terpri)
 
-	; count how many we need to add, and add them
-	(setf fish-to-populate (apply '+ (loop for f in fish collect (if (= f -1)(eval 1)(eval 0)))))
-	(set 'fish (append fish (make-list fish-to-populate :initial-element 8)))
-
-	; reset timers
-	(set 'fish (map 'list (lambda (x) (reset-timer x)) fish))
-	(format T "After ~d days, there are ~d fish." day (length fish))(terpri)))
-
-
+      
 
