ae1802cce06d2c330e6397cdd45a240319b066d3
Grant Gordon <grant.gordon@hey.com>
2021-12-08 19:17:42 -0500

    clean up solution for part 2

--- day3/puzzle3.lisp
+++ day3/puzzle3.lisp
@@ -1,4 +1,3 @@
-
 (defvar puzzle-input)
 (set 'puzzle-input
      (with-open-file (stream "puzzle3.input")
@@ -9,44 +8,36 @@
 (defun get-popular-at-n (l n default)
   (declare (list l))
   (declare (number n))
-  (declare (character default))
-  (terpri)
 
   (let ((number-of-zeroes (length (remove-if (lambda (x) (equal (aref x n) #\1)) l)))
 	(half-of-list (/ (length l) 2)))
-    (format T "number-of-zeroes ~d , half-of-list ~d" number-of-zeroes half-of-list)(terpri)
     (cond ((> number-of-zeroes half-of-list)(eval #\0))
 	  ((< number-of-zeroes half-of-list)(eval #\1))
-	  ((= number-of-zeroes half-of-list)(print "equally common")(eval default)))))
+	  ((= number-of-zeroes half-of-list)(eval default)))))
 
-; part 1
-; (loop for i from 0 upto 11 do
-      ; (print (get-popular-at-n puzzle-input i)))
+; part 1 - print these out and manually reverse
+; (loop for i from 0 upto 11 do (print (get-popular-at-n puzzle-input i #\1)))
 
 
 
 ; part 2
+(defun calculate-rating (l type-of-rating)
+  (declare (list l))
+  (declare (string type-of-rating))
 
-; oxygen generator rating
-; keep numbers with the most common value, default to 1 if equally common
-(let ((filter-list puzzle-input))
-  (loop for n from 0 upto 11 do
-	(let ((popular-at-n (get-popular-at-n filter-list n #\1)))
-	  (format T "popular at ~d is ~d" n popular-at-n)(terpri)
-	  (format T "before filtering, ~d entries, " (length filter-list))
-	  (setf filter-list (remove-if-not (lambda(x) (equal popular-at-n (aref x n))) filter-list))
-	  (format T "after filtering, ~d entries" (length filter-list))(terpri)
-	  (if (equal 1 (length filter-list))(print filter-list)()))))
+  (let ((filter-list l))
+    (loop for n from 0 upto (- (length filter-list) 1)
+	  until (= 1 (length filter-list)) do
+	  (if (equal "o2" type-of-rating)
+	    (setf filter-list 
+		  (remove-if-not (lambda(x) (equal (get-popular-at-n filter-list n #\1) (aref x n))) filter-list))
+	    (setf filter-list 
+		  (remove-if     (lambda(x) (equal (get-popular-at-n filter-list n #\1) (aref x n))) filter-list))))
+    (car filter-list)))
 
-(terpri)(terpri)
+; print these out and manually convert and multiply them
+(print (calculate-rating puzzle-input "o2"))
+(print (calculate-rating puzzle-input "co2"))
 
-; co2 scrubber rating
-; keep numbers with the least common value, default to 0 if equally common
-(let ((filter-list puzzle-input))
-  (loop for n from 0 upto 11 do
-	(let ((popular-at-n (get-popular-at-n filter-list n #\1)))
-	  (format T "popular at ~d is ~d" n popular-at-n)(terpri)
-	  (format T "before filtering, ~d entries, " (length filter-list))
-	  (setf filter-list (remove-if (lambda(x) (equal popular-at-n (aref x n))) filter-list))
-	  (format T "after filtering, ~d entries" (length filter-list))(terpri)
-	  (if (equal 1 (length filter-list))(print filter-list)()))))
+; https://stackoverflow.com/questions/34219181/binary-to-decimal-in-lisp-using-a-non-nested-list
+
