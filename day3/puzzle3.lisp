7cff3710b56a94ab57e7490958b9436e568196e3
Grant Gordon <grant.gordon@hey.com>
2021-12-08 19:24:45 -0500

    swap out an if for a cond

--- day3/puzzle3.lisp
+++ day3/puzzle3.lisp
@@ -18,26 +18,28 @@
 ; part 1 - print these out and manually reverse
 ; (loop for i from 0 upto 11 do (print (get-popular-at-n puzzle-input i #\1)))
 
-
-
 ; part 2
 (defun calculate-rating (l type-of-rating)
   (declare (list l))
   (declare (string type-of-rating))
 
   (let ((filter-list l))
-    (loop for n from 0 upto (- (length filter-list) 1)
-	  until (= 1 (length filter-list)) do
-	  (if (equal "o2" type-of-rating)
-	    (setf filter-list 
-		  (remove-if-not (lambda(x) (equal (get-popular-at-n filter-list n #\1) (aref x n))) filter-list))
-	    (setf filter-list 
-		  (remove-if     (lambda(x) (equal (get-popular-at-n filter-list n #\1) (aref x n))) filter-list))))
+    (loop 
+      for n from 0 upto (- (length filter-list) 1)
+      until (= 1 (length filter-list)) 
+      do
+      (cond ((equal "o2" type-of-rating)
+	     (setf filter-list 
+		   (remove-if-not (lambda(x) (equal (get-popular-at-n filter-list n #\1) (aref x n))) filter-list)))
+	    ((equal "co2" type-of-rating)
+	     (setf filter-list 
+		   (remove-if (lambda(x) (equal (get-popular-at-n filter-list n #\1) (aref x n))) filter-list)))))
     (car filter-list)))
 
 ; print these out and manually convert and multiply them
 (print (calculate-rating puzzle-input "o2"))
 (print (calculate-rating puzzle-input "co2"))
 
+; If I want to code up converting and multiplying the binary strings, this provides an option. 
 ; https://stackoverflow.com/questions/34219181/binary-to-decimal-in-lisp-using-a-non-nested-list
 
